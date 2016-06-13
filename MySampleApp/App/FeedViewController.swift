ace6b3f326c13bff0b222f65e442f4eea6248bd0
Steven Schmatz <stevenschmatz@gmail.com>
2016-06-13 16:55:53 -0700

    new project

---
+++ MySampleApp/App/FeedViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FeedViewController.swift
+//  Mist
+//
+//  Created by Steven on 6/13/16.
+//
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .whiteColor()
+    }
+}
--- Pods/Target Support Files/Pods-Mist/Pods-Mist-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Mist : NSObject
-@end
-@implementation PodsDummy_Pods_Mist
-@end
