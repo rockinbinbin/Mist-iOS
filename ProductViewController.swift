9501a856bb8d2a63a10b5ec6fdec00363c4bd7bc
Robin <robimeht@umich.edu>
2016-07-03 15:38:13 -0600

    deleted comment

--- ProductViewController.swift
+++ ProductViewController.swift
@@ -73,8 +73,6 @@ class ProductViewController: UIViewController, PKPaymentAuthorizationViewControl
                 return
             }
             
-            // convert URLs to images TODONOW
-            
             for string in array {
                 
                 dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
